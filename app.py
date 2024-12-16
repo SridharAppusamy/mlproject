20e56e49ec88e6daa0e1b3da8c3e267ab96e457e
sridharappusamy <sridharappusamy@gmail.com>
2024-12-16 17:34:09 +0530

    secnd

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
